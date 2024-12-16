6faebe0f95b9ba84fc0bc756585644a2ba189096
dryan-cais <dryan@caisgroup.com>
2024-12-16 22:12:46 +0000

    Fix paths again

--- generate.sh
+++ generate.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Convert Markdown to Word 
-docker run --platform linux/amd64 --rm -v "$(pwd):/data" pandoc/latex /data/cv.md -o /data/cv.docx --reference-doc=/templates/custom.docx
+docker run --platform linux/amd64 --rm -v "$(pwd):/data" pandoc/latex /data/cv.md -o /data/cv.docx --reference-doc=/data/templates/custom.docx
 
 # Convert Markdown to PDF
-docker run --platform linux/amd64 --rm -v "$(pwd):/data" pandoc/latex /data/cv.md -o /data/cv.pdf --template=/templates/custom.latex
+docker run --platform linux/amd64 --rm -v "$(pwd):/data" pandoc/latex /data/cv.md -o /data/cv.pdf --template=/data/templates/custom.latex
 
 echo "Conversion complete! Check cv.docx and cv.pdf in your directory."
